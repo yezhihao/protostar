56739ffc88e9d51d0ea6658e7b613f89643b4339
剑器近 <zhihao.ye@qq.com>
2021-07-02 15:05:19 +0800

    PrepareLoadStrategy.addSchema DWORD修改为Int32

--- src/main/java/io/github/yezhihao/protostar/PrepareLoadStrategy.java
+++ src/main/java/io/github/yezhihao/protostar/PrepareLoadStrategy.java
@@ -42,7 +42,7 @@ public abstract class PrepareLoadStrategy extends IdStrategy {
                 this.typeIdMapping.put(key, NumberSchema.Int16.INSTANCE);
                 break;
             case DWORD:
-                this.typeIdMapping.put(key, NumberSchema.Long32.INSTANCE);
+                this.typeIdMapping.put(key, NumberSchema.Int32.INSTANCE);
                 break;
             case QWORD:
                 this.typeIdMapping.put(key, NumberSchema.Long64.INSTANCE);
