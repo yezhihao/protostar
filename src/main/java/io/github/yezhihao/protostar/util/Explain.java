29c5e02fe7ee9b77500675801eb9e24a61297a9e
剑器近 <zhihao.ye@qq.com>
2021-12-19 18:29:58 +0800

    update

--- src/main/java/io/github/yezhihao/protostar/util/Explain.java
+++ src/main/java/io/github/yezhihao/protostar/util/Explain.java
@@ -1,5 +1,8 @@
 package io.github.yezhihao.protostar.util;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,14 +13,24 @@ import java.util.List;
  */
 public class Explain {
 
+    public static final Empty Empty = new Empty();
+
     private List<Info> list;
 
     public Explain() {
         this.list = new LinkedList<>();
     }
 
-    public void add(Info info) {
-        list.add(info);
+    public void readField(int index, String desc, Object value, ByteBuf input) {
+        list.add(Info.field(index, desc, value, ByteBufUtil.hexDump(input, index, input.readerIndex() - index)));
+    }
+
+    public void writeField(int index, String desc, Object value, ByteBuf output) {
+        list.add(Info.field(index, desc, value, ByteBufUtil.hexDump(output, index, output.writerIndex() - index)));
+    }
+
+    public void lengthField(int index, String desc, int value) {
+        list.add(Info.lengthField(index, desc, value));
     }
 
     public List<Info> getList() {
@@ -37,6 +50,24 @@ public class Explain {
             Object value = info.getValue();
             if (value != null)
                 System.out.println(info.getIndex() + "\t" + "[" + info.getRaw() + "] " + info.getDesc() + ": " + StrUtils.toString(value));
+        }
+    }
+
+    private static class Empty extends Explain {
+        @Override
+        public void readField(int index, String desc, Object value, ByteBuf input) {
+        }
+
+        @Override
+        public void writeField(int index, String desc, Object value, ByteBuf output) {
+        }
+
+        @Override
+        public void lengthField(int index, String desc, int value) {
+        }
+
+        @Override
+        public void setLastDesc(String desc) {
         }
     }
 }
