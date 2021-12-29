39be3b8704866ad4285698701532737f66fdd8f6
剑器近 <zhihao.ye@qq.com>
2021-12-29 21:53:44 +0800

    修复StringSchema缺陷

--- src/main/java/io/github/yezhihao/protostar/schema/StringSchema.java
+++ src/main/java/io/github/yezhihao/protostar/schema/StringSchema.java
@@ -6,21 +6,18 @@ import io.github.yezhihao.protostar.field.LengthUnitField;
 import io.github.yezhihao.protostar.util.CharsBuilder;
 import io.netty.buffer.ByteBuf;
 import io.netty.util.internal.StringUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class StringSchema {
-
-    private static final Logger log = LoggerFactory.getLogger(StringSchema.class.getSimpleName());
 
     public static final Schema<String> HEX = new HEX(-1);
     public static final Schema<String> BCD = new BCD(-1);
     public static final Schema<String> GBK = new STR(Charset.forName("GBK"), -1);
-    public static final Schema<String> UTF8 = new STR(Charset.forName("UTF-8"), -1);
-    public static final Schema<String> ASCII = new STR(Charset.forName("US-ASCII"), -1);
+    public static final Schema<String> UTF8 = new STR(StandardCharsets.UTF_8, -1);
+    public static final Schema<String> ASCII = new STR(StandardCharsets.US_ASCII, -1);
 
     public static BasicField<String> getInstance(String charset, int length, int lengthUnit) {
         final String cs = charset.toUpperCase();
@@ -39,47 +36,42 @@ public class StringSchema {
     }
 
     public static class STR extends BasicField<String> {
-        private final byte pad = 0;
+        private static final ByteBuffer EMPTY = ByteBuffer.allocate(0);
         private final Charset charset;
         private final int length;
+        private final boolean fixed;
 
         private STR(Charset charset, int length) {
             this.charset = charset;
             this.length = length;
+            this.fixed = length > -1;
         }
 
         @Override
         public String readFrom(ByteBuf input) {
-            return readFrom(input, length);
-        }
+            int len = input.readableBytes();
 
-        @Override
-        public String readFrom(ByteBuf input, int length) {
-            int len = length >= 0 ? length : input.readableBytes();
-            if (!input.isReadable(len))
-                return null;
+            if (fixed && len > length)
+                len = length;
+
             byte[] bytes = new byte[len];
             input.readBytes(bytes);
 
             int st = 0;
-            while ((st < len) && (bytes[st] == pad))
+            while ((st < len) && (bytes[st] == 0))
                 st++;
-            while ((st < len) && (bytes[len - 1] == pad))
+            while ((st < len) && (bytes[len - 1] == 0))
                 len--;
             return new String(bytes, st, len - st, charset);
         }
 
         @Override
         public void writeTo(ByteBuf output, String value) {
-            writeTo(output, length, value);
-        }
+            if (fixed) {
+                ByteBuffer buffer;
+                if (value == null) buffer = EMPTY;
+                else buffer = charset.encode(value);
 
-        @Override
-        public void writeTo(ByteBuf output, int length, String value) {
-            if (value == null)
-                return;
-            ByteBuffer buffer = charset.encode(value);
-            if (length > 0) {
                 int srcPos = length - buffer.limit();
 
                 if (srcPos > 0) {
@@ -88,105 +80,76 @@ public class StringSchema {
                 } else if (srcPos < 0) {
                     buffer.position(-srcPos);
                     output.writeBytes(buffer);
-                    log.info("字符长度超出限制: 长度[{}],数据长度[{}],{}", length, buffer.limit(), value);
                 } else {
                     output.writeBytes(buffer);
                 }
             } else {
-                output.writeBytes(buffer);
+                if (value != null)
+                    output.writeBytes(charset.encode(value));
             }
-        }
-    }
-
-    public static class BCD extends BasicField<String> {
-        private final int length;
-
-        public BCD(int length) {
-            this.length = length;
-        }
-
-        @Override
-        public String readFrom(ByteBuf input) {
-            return readFrom(input, length);
-        }
-
-        @Override
-        public String readFrom(ByteBuf input, int length) {
-            int len = length >= 0 ? length : input.readableBytes();
-            byte[] bytes = new byte[len];
-            input.readBytes(bytes);
-
-            CharsBuilder cb = new CharsBuilder(length << 1);
-            StringUtil.toHexStringPadded(cb, bytes);
-            return cb.leftStrip('0');
-        }
-
-        @Override
-        public void writeTo(ByteBuf output, String value) {
-            writeTo(output, length, value);
-        }
-
-        @Override
-        public void writeTo(ByteBuf output, int length, String value) {
-            int charLength = length << 1;
-            char[] chars = new char[charLength];
-            int i = charLength - value.length();
-            if (i >= 0) {
-                value.getChars(0, charLength - i, chars, i);
-                while (i > 0)
-                    chars[--i] = '0';
-            } else {
-                value.getChars(-i, charLength - i, chars, 0);
-                log.info("字符长度超出限制: 长度[{}],[{}]", charLength, value);
-            }
-            byte[] src = StringUtil.decodeHexDump(new CharsBuilder(chars));
-            output.writeBytes(src);
         }
     }
 
     public static class HEX extends BasicField<String> {
-        private final int length;
+        protected final int length;
+        protected final int charSize;
+        protected final boolean fixed;
 
         public HEX(int length) {
             this.length = length;
+            this.charSize = length << 1;
+            this.fixed = length > -1;
         }
 
         @Override
         public String readFrom(ByteBuf input) {
-            return readFrom(input, length);
+            return readCharsBuilder(input).toString();
         }
 
-        @Override
-        public String readFrom(ByteBuf input, int length) {
-            int len = length >= 0 ? length : input.readableBytes();
+        protected CharsBuilder readCharsBuilder(ByteBuf input) {
+            int len = fixed ? length : input.readableBytes();
             byte[] bytes = new byte[len];
             input.readBytes(bytes);
 
-            CharsBuilder cb = new CharsBuilder(length << 1);
+            CharsBuilder cb = new CharsBuilder(charSize);
             StringUtil.toHexStringPadded(cb, bytes);
-            return cb.toString();
+            return cb;
         }
 
         @Override
         public void writeTo(ByteBuf output, String value) {
-            writeTo(output, length, value);
-        }
+            if (value == null) {
+                if (fixed) output.writeBytes(new byte[length]);
+                return;
+            }
 
-        @Override
-        public void writeTo(ByteBuf output, int length, String value) {
-            int charLength = length << 1;
-            char[] chars = new char[charLength];
-            int i = charLength - value.length();
+            int charSize = this.charSize;
+            int strLength = value.length();
+
+            if (!fixed) charSize = strLength + (strLength & 1);
+
+            char[] chars = new char[charSize];
+            int i = charSize - strLength;
             if (i >= 0) {
-                value.getChars(0, charLength - i, chars, i);
+                value.getChars(0, charSize - i, chars, i);
                 while (i > 0)
                     chars[--i] = '0';
             } else {
-                value.getChars(-i, charLength - i, chars, 0);
-                log.info("字符长度超出限制: 长度[{}],[{}]", charLength, value);
+                value.getChars(-i, charSize - i, chars, 0);
             }
             byte[] src = StringUtil.decodeHexDump(new CharsBuilder(chars));
             output.writeBytes(src);
+        }
+    }
+
+    public static class BCD extends HEX {
+        public BCD(int length) {
+            super(length);
+        }
+
+        @Override
+        public String readFrom(ByteBuf input) {
+            return readCharsBuilder(input).leftStrip('0');
         }
     }
 }
